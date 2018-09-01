c41e06e659e4de39a79528340408e26eef638540
Worapot <plamworapot@gmail.com>
2018-09-01 14:28:47 +0700

    chanbge gh page

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 
 import '@app/assets/less/styles.less'
 
-export default ({ children }) =>
+const assetPrefix = process.env.ASSET_PREFIX
+
+export default ({ children, __NEXT_DATA__ }) =>
   <div>
     <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta charSet='utf-8' />
-      <link rel='stylesheet' href='/_next/static/style.css' />
+      <link rel='stylesheet' href={`${assetPrefix}/_next/static/style.css`} />
     </Head>
     <style jsx global>{`
       body {
